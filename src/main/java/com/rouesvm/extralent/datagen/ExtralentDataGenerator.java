4fec7c92a64a644c19440d792b395e44de94d275
drivlo <rouesvmnowfree@gmail.com>
2024-12-04 20:27:33 +0800

    Last commit

--- src/main/java/com/rouesvm/extralent/datagen/ExtralentDataGenerator.java
+++ src/main/java/com/rouesvm/extralent/datagen/ExtralentDataGenerator.java
@@ -1,6 +1,5 @@
 package com.rouesvm.extralent.datagen;
 
-import com.rouesvm.extralent.datagen.model.ModelGenerator;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -8,6 +7,5 @@ public class ExtralentDataGenerator implements DataGeneratorEntrypoint {
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator generator) {
         FabricDataGenerator.Pack pack = generator.createPack();
-        pack.addProvider(ModelGenerator::new);
     }
 }
