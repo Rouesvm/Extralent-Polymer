b367455ff0d0af34cb19e58d2d3f29a9f84100b2
drivlo <rouesvmnowfree@gmail.com>
2024-12-06 21:42:40 +0800

    Add recipes for blocks and items.

--- src/main/java/com/rouesvm/extralent/datagen/ModRecipeGenerator.java
+++ src/main/java/com/rouesvm/extralent/datagen/ModRecipeGenerator.java
@@ -1,6 +1,7 @@
 package com.rouesvm.extralent.datagen;
 
 import com.rouesvm.extralent.registries.block.BlockRegistry;
+import com.rouesvm.extralent.registries.item.ItemRegistry;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.advancement.criterion.InventoryChangedCriterion;
@@ -9,6 +10,8 @@ import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.registry.tag.TagKey;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -19,11 +22,39 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
 
     @Override
     public void generate(RecipeExporter exporter) {
+        blockRecipes(exporter);
+        itemRecipes(exporter);
+    }
+
+    private void blockRecipes(RecipeExporter exporter) {
         ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, BlockRegistry.GENERATOR, 1)
                 .pattern("rir").pattern("ifi").pattern("ccc")
                 .input('r', Items.REDSTONE).input('i', Items.IRON_INGOT)
                 .input('f', Items.FURNACE).input('c', Items.COPPER_BLOCK)
                 .criterion("get_redstone", InventoryChangedCriterion.Conditions.items(Items.REDSTONE))
+                .offerTo(exporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, BlockRegistry.TRANSMITTER, 4)
+                .pattern("rir").pattern("grg").pattern("rir")
+                .input('r', Items.REDSTONE).input('g', Items.GOLD_BLOCK)
+                .input('i', Items.COPPER_INGOT)
+                .criterion("get_redstone", InventoryChangedCriterion.Conditions.items(Items.REDSTONE))
+                .offerTo(exporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, BlockRegistry.TRANSPORTER, 6)
+                .pattern("rir").pattern("iei").pattern("rir")
+                .input('r', Items.COPPER_INGOT).input('e', Items.ENDER_PEARL)
+                .input('i', ItemTags.PLANKS)
+                .criterion("get_pearl", InventoryChangedCriterion.Conditions.items(Items.ENDER_PEARL))
+                .offerTo(exporter);
+    }
+
+    private void itemRecipes(RecipeExporter exporter) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ItemRegistry.CONNECTOR, 1)
+                .pattern(" i ").pattern("bbb").pattern("bcb")
+                .input('c', BlockRegistry.TRANSMITTER).input('b', Items.COPPER_INGOT)
+                .input('i', Items.LIGHTNING_ROD)
+                .criterion("get_pearl", InventoryChangedCriterion.Conditions.items(Items.ENDER_PEARL))
                 .offerTo(exporter);
     }
 
