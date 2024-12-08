9c17eeed360b3b84e8d58d36a230d913aa8d5870
drivlo <rouesvmnowfree@gmail.com>
2024-12-08 21:14:34 +0800

    Added new variable called Direction to be more specific with inputting.

--- src/main/java/com/rouesvm/extralent/block/transport/entity/connection/Connection.java
+++ src/main/java/com/rouesvm/extralent/block/transport/entity/connection/Connection.java
@@ -1,24 +1,35 @@
-package com.rouesvm.extralent.utils;
+package com.rouesvm.extralent.block.transport.entity.connection;
 
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 
 import java.util.HashSet;
 import java.util.Objects;
 
 public class Connection {
     private final BlockPos pos;
+    private Direction side;
     private int weight;
 
-    protected Connection(BlockPos pos, int weight) {
+    protected Connection(BlockPos pos, int weight, Direction side) {
         this.pos = pos;
         this.weight = weight;
+        this.side = side;
+    }
+
+    public static Connection of(BlockPos pos, int weight, Direction side) {
+        return new Connection(pos, weight, side);
     }
 
     public static Connection of(BlockPos pos, int weight) {
-        return new Connection(pos, weight);
+        return new Connection(pos, weight, Direction.UP);
+    }
+
+    public Direction getSide() {
+        return side;
     }
 
     public BlockPos getPos() {
@@ -38,6 +49,7 @@ public class Connection {
 
         connections.forEach(connection -> {
             NbtCompound nbtCompound = new NbtCompound();
+            nbtCompound.putInt("side", connection.getSide().getId());
             nbtCompound.putInt("weight", connection.getWeight());
             nbtCompound.putLong("pos", connection.getPos().asLong());
             nbtList.add(nbtCompound);
@@ -51,9 +63,10 @@ public class Connection {
 
         for(int i = 0; i < nbtList.size(); ++i) {
             NbtCompound nbtCompound = nbtList.getCompound(i);
-            long l = nbtCompound.getLong("pos");
-            int j = nbtCompound.getInt("weight");
-            connections.add(of(BlockPos.fromLong(l), j));
+            int side = nbtCompound.getInt("side");
+            long pos = nbtCompound.getLong("pos");
+            int weight = nbtCompound.getInt("weight");
+            connections.add(of(BlockPos.fromLong(pos), weight, Direction.byId(side)));
         }
     }
 
