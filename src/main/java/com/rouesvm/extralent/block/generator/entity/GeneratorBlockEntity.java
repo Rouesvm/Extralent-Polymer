ee70a95ae99e0fdb966d267f48ca3b0574a9759c
drivlo <rouesvmnowfree@gmail.com>
2024-12-11 14:55:25 +0800

    Add a charging slot.

--- src/main/java/com/rouesvm/extralent/block/generator/entity/GeneratorBlockEntity.java
+++ src/main/java/com/rouesvm/extralent/block/generator/entity/GeneratorBlockEntity.java
@@ -5,7 +5,11 @@ import com.rouesvm.extralent.block.entity.BasicMachineBlockEntity;
 import com.rouesvm.extralent.registries.block.BlockEntityRegistry;
 import com.rouesvm.extralent.ui.inventory.ExtralentInventory;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.fabricmc.fabric.api.transfer.v1.context.ContainerItemContext;
+import net.fabricmc.fabric.api.transfer.v1.item.InventoryStorage;
+import net.fabricmc.fabric.api.transfer.v1.storage.base.SingleSlotStorage;
 import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
+import net.fabricmc.fabric.impl.transfer.context.SingleSlotContainerItemContext;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.item.ItemStack;
@@ -15,11 +19,19 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.MathHelper;
 import team.reborn.energy.api.EnergyStorage;
+import team.reborn.energy.api.EnergyStorageUtil;
+import team.reborn.energy.api.base.SimpleEnergyItem;
 import team.reborn.energy.api.base.SimpleEnergyStorage;
+
+import java.util.Arrays;
 
 public class GeneratorBlockEntity extends BasicMachineBlockEntity {
     private int progress;
     private int burnTime;
+
+    public static final int CHARGING_SLOT_INDEX = 1;
+
+    private static final int[] INPUT_SLOTS_ARRAY = {0};
 
     public GeneratorBlockEntity(BlockPos pos, BlockState state) {
         super(BlockEntityRegistry.GENERATOR_BLOCK_ENTITY, pos, state);
@@ -27,7 +39,28 @@ public class GeneratorBlockEntity extends BasicMachineBlockEntity {
 
     @Override
     public ExtralentInventory createInventory() {
-        return super.createInventory(1);
+        return new ExtralentInventory(2) {
+            @Override
+            public void markDirty() {
+                super.markDirty();
+                update();
+            }
+
+            @Override
+            public int[] getAvailableSlots(Direction side) {
+                return INPUT_SLOTS_ARRAY;
+            }
+
+            @Override
+            public boolean canInsert(int slot, ItemStack stack, Direction dir) {
+                return slot == 0;
+            }
+
+            @Override
+            public boolean canExtract(int slot, ItemStack stack, Direction dir) {
+                return slot == 0;
+            }
+        };
     }
 
     @Override
@@ -60,7 +93,17 @@ public class GeneratorBlockEntity extends BasicMachineBlockEntity {
         extractEnergy();
     }
 
-    public void extractEnergy() {
+    private void extractEnergy() {
+        ItemStack stack = inventory.getStack(CHARGING_SLOT_INDEX);
+        if (!stack.isEmpty()) {
+            EnergyStorageUtil.move(
+                    getEnergyProvider(null),
+                    ContainerItemContext.ofSingleSlot(InventoryStorage.of(inventory, null).getSlots().get(CHARGING_SLOT_INDEX)).find(EnergyStorage.ITEM),
+                    Long.MAX_VALUE,
+                    null
+            );
+        }
+
         for (Direction direction : Direction.values()) {
             EnergyStorage storage = EnergyStorage.SIDED.find(this.world, this.pos.offset(direction), direction.getOpposite());
             if (storage != null && storage.supportsInsertion()) {
@@ -78,7 +121,7 @@ public class GeneratorBlockEntity extends BasicMachineBlockEntity {
         ItemStack fuelStack = this.inventory.getStack(0);
         if (this.progress == 0 && !fuelStack.isEmpty()) {
             var burning = FuelRegistry.INSTANCE.get(fuelStack.getItem());
-            if (burning != 0) {
+            if (burning != null && burning != 0) {
                 fuelStack.decrement(1);
                 this.inventory.setStack(0, fuelStack);
                 this.burnTime = burning;
