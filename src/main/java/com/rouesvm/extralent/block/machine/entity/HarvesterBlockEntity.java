2142deb283fe2f35eec5078922f8e96e31083f90
drivlo <rouesvmnowfree@gmail.com>
2024-12-28 17:03:53 +0800

    Optimize a bit and make Harvester plant saplings every tick.

--- src/main/java/com/rouesvm/extralent/block/machine/entity/HarvesterBlockEntity.java
+++ src/main/java/com/rouesvm/extralent/block/machine/entity/HarvesterBlockEntity.java
@@ -2,36 +2,36 @@ package com.rouesvm.extralent.block.machine.entity;
 
 import com.rouesvm.extralent.block.MachineBlock;
 import com.rouesvm.extralent.block.entity.BasicMachineBlockEntity;
+import com.rouesvm.extralent.block.transport.entity.connection.Connection;
 import com.rouesvm.extralent.registries.block.BlockEntityRegistry;
+import com.rouesvm.extralent.registries.block.BlockRegistry;
 import com.rouesvm.extralent.ui.inventory.ExtralentInventory;
 import com.rouesvm.extralent.visual.LineDrawer;
 import net.fabricmc.fabric.api.transfer.v1.item.InventoryStorage;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.SaplingBlock;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.loot.context.LootContextParameterSet;
 import net.minecraft.loot.context.LootContextParameters;
-import net.minecraft.particle.DustParticleEffect;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.CuboidBlockIterator;
+import net.minecraft.util.math.*;
 import net.minecraft.world.World;
-import org.joml.Vector3f;
 import team.reborn.energy.api.base.SimpleEnergyStorage;
 
 import java.util.*;
 
 public class HarvesterBlockEntity extends BasicMachineBlockEntity {
-    private static final int width = 8;
-    private static final int height = 8;
-    private static final int depth = 8;
+    private static final Vec3i boxSize = new Vec3i(8, 8, 8);
 
     public static final long ENERGY_USED = 500;
 
@@ -40,6 +40,11 @@ public class HarvesterBlockEntity extends BasicMachineBlockEntity {
 
     private int ticks;
     private final InventoryStorage outputInventory;
+
+    private final HashSet<BlockPos> soilPos = new HashSet<>(boxSize.getX() * boxSize.getZ() / 2);
+    private HashSet<BlockPos> cachedPos = null;
+
+    private Queue<BlockPos> soilQueue = new LinkedList<>();
 
     public HarvesterBlockEntity(BlockPos pos, BlockState state) {
         super(BlockEntityRegistry.HARVESTER_BLOCK_ENTITY, pos, state);
@@ -88,59 +93,116 @@ public class HarvesterBlockEntity extends BasicMachineBlockEntity {
         Block machineBlock = getCachedState().getBlock();
         if (!(machineBlock instanceof MachineBlock machineBaseBlock)) return;
 
+        if (cachedPos == null) {
+            cachedPos = new HashSet<>(boxSize.getX() * boxSize.getY() * boxSize.getZ());
+            cacheArea();
+        }
+
         if (energyStorage.amount <= ENERGY_USED) {
             machineBaseBlock.setState(false, world, pos);
             return;
+        } else machineBaseBlock.setState(true, world, pos);
+
+        ticks++;
+        if (ticks % 6 == 0) {
+            plantSaplings(world);
+            energyStorage.amount = MathHelper.clamp(energyStorage.amount - ENERGY_USED / ((long) boxSize.getX() * boxSize.getZ() / 2), 0, energyStorage.getCapacity());
+            markDirty();
         }
 
-        if (ticks++ % 20 == 0) {
-            ticks = 0;
-            scanArea(pos, world);
-
+        if (ticks % 80 == 0) {
+            scanAreaForSoil(world);
             energyStorage.amount = MathHelper.clamp(energyStorage.amount - ENERGY_USED, 0, energyStorage.getCapacity());
-            machineBaseBlock.setState(true, world, pos);
+            markDirty();
         }
     }
 
-    public void scanArea(BlockPos machinePos, World world) {
-        int xStart = machinePos.getX() - width / 2;
-        int zStart = machinePos.getZ() - depth / 2;
-        int yStart = machinePos.getY() + 1;
-        int yEnd = yStart + height;
+    private void cacheArea() {
+        int xStart = pos.getX() - boxSize.getX() / 2;
+        int zStart = pos.getZ() - boxSize.getZ() / 2;
+        int yStart = pos.getY() + 1;
+        int yEnd = yStart + boxSize.getY();
 
-        for (int x = xStart; x <= xStart + width; x++) {
-            for (int z = zStart; z <= zStart + depth; z++) {
+        for (int x = xStart; x <= xStart + boxSize.getX(); x++) {
+            for (int z = zStart; z <= zStart + boxSize.getZ(); z++) {
                 for (int y = yStart; y < yEnd; y++) {
-                    BlockPos pos = new BlockPos(x, y, z);
-                    BlockState state = world.getBlockState(pos);
-                    if (isLog(state)) {
-                        harvestTree(world, pos);
-                    } else if (isGroundSuitable(world.getBlockState(pos.down())) && isAirAbove(world, pos.down())) {
-                        plantSapling(world, pos.down());
-                    }
+                    cachedPos.add(new BlockPos(x, y, z));
                 }
             }
         }
+    }
+
+    private void plantSaplings(World world) {
+        if (!soilQueue.isEmpty()) {
+            BlockPos pos = soilQueue.poll();
+            boolean didPlant = plantSapling(world, pos.down());
+            if (!didPlant) plantSaplings(world);
+        }
+    }
+
+    private void scanAreaForSoil(World world) {
+        cachedPos.forEach(pos -> {
+            BlockState state = world.getBlockState(pos);
+            BlockState downState = world.getBlockState(pos.down());
+            if (isBreakableBlock(state))
+                harvestTree(world, pos);
+            if (isGroundSuitable(downState) && world.isAir(pos.up()))
+                soilPos.add(pos);
+            else soilPos.remove(pos);
+        });
+
+        if (soilQueue.isEmpty()) soilQueue.addAll(soilPos);
     }
 
     private void harvestTree(World world, BlockPos pos) {
         Queue<BlockPos> toCheck = new LinkedList<>();
         toCheck.add(pos);
 
+        boolean playSound = false;
+
         while (!toCheck.isEmpty()) {
             BlockPos current = toCheck.poll();
             BlockState state = world.getBlockState(current);
-            if (isLog(state)) {
-                insertDrops(getDrops(state, current));
-                world.breakBlock(current, false);
-                for (Direction direction : Direction.values()) {
-                    BlockPos neighbor = current.offset(direction);
-                    if (isLog(world.getBlockState(neighbor))) {
-                        toCheck.add(neighbor);
-                    }
+            if (!isBreakableBlock(state)) continue;
+
+            if (!playSound) {
+                playSound = true;
+                world.playSound(null, pos, SoundEvents.BLOCK_WOOD_BREAK, SoundCategory.BLOCKS, 2f, 1f);
+            }
+
+            insertDrops(getDrops(state, current));
+            world.setBlockState(current, Blocks.AIR.getDefaultState());
+            for (Direction direction : Direction.values()) {
+                BlockPos neighbor = current.offset(direction);
+                if (isBreakableBlock(world.getBlockState(neighbor))) {
+                    toCheck.add(neighbor);
                 }
             }
         }
+    }
+
+    private boolean plantSapling(World world, BlockPos pos) {
+        if (world.isAir(pos)) return false;
+        if (!world.isAir(pos.up())) return false;
+        if (inventory.isEmpty()) return true;
+
+        Item selectedSapling = inventory.getStacks().stream()
+                .filter(stack -> stack.isIn(ItemTags.SAPLINGS))
+                .map(ItemStack::getItem)
+                .findFirst()
+                .orElse(null);
+
+        if (selectedSapling != null) {
+            ItemStack removedStack = inventory.removeItem(selectedSapling, 1);
+            if (!removedStack.isEmpty()) {
+                Block saplingBlock = Block.getBlockFromItem(selectedSapling);
+                world.playSound(null, pos.up(), SoundEvents.BLOCK_GRASS_PLACE, SoundCategory.BLOCKS, 1f, 1f);
+                world.setBlockState(pos.up(), saplingBlock.getDefaultState());
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private List<ItemStack> getDrops(BlockState state, BlockPos current) {
@@ -152,93 +214,24 @@ public class HarvesterBlockEntity extends BasicMachineBlockEntity {
     }
 
     private void insertDrops(List<ItemStack> drops) {
-        drops.forEach(itemStack -> {
-            ItemStack stack = itemStack.copy();
-
-            for (int slot : OUTPUT_SLOTS_ARRAY) {
-                ItemStack slotStack = inventory.getStack(slot);
-                if (slotStack.isEmpty() || ItemStack.areItemsEqual(slotStack, stack)) {
-                    int availableSpace = 64 - slotStack.getCount();
-                    int toInsert = Math.min(stack.getCount(), availableSpace);
-
-                    if (slotStack.isEmpty()) {
-                        ItemStack newStack = stack.copy();
-                        newStack.setCount(toInsert);
-                        inventory.setStack(slot, newStack);
-                    } else slotStack.increment(toInsert);
-
-                    stack.decrement(toInsert);
-                    itemStack.decrement(toInsert);
-
-                    if (stack.isEmpty()) break;
-                }
-            }
+        drops.forEach(drop -> {
+            if (drop.isIn(ItemTags.SAPLINGS))
+                drop = inventory.insertStack(drop, INPUT_SLOTS_ARRAY);
+            inventory.insertStack(drop, OUTPUT_SLOTS_ARRAY);
         });
-        drops.removeIf(ItemStack::isEmpty);
     }
 
-    private boolean isLog(BlockState state) {
+    private boolean isBreakableBlock(BlockState state) {
         return state.isIn(BlockTags.LOGS) || state.isIn(BlockTags.LEAVES);
     }
 
-    private boolean isAirAbove(World world, BlockPos pos) {
-        return world.isAir(pos.up());
-    }
-
-    private void plantSapling(World world, BlockPos pos) {
-        if (world.isAir(pos.up())) {
-            if (inventory.isEmpty()) return;
-            Item selectedSapling = null;
-            for (ItemStack stack : inventory.getStacks()) {
-                if (stack.isIn(ItemTags.SAPLINGS)) selectedSapling = stack.getItem();
-            }
-
-            if (selectedSapling != null
-                    && !inventory.removeItem(selectedSapling, 1).isEmpty()
-            ) {
-                Block saplingBlock = Block.getBlockFromItem(selectedSapling);
-                world.setBlockState(pos.up(), saplingBlock.getDefaultState());
-            }
-        }
-    }
-
     private boolean isGroundSuitable(BlockState state) {
-        return state.isOf(Blocks.DIRT) || state.isOf(Blocks.GRASS_BLOCK);
-    }
-
-    private void visualizeScanArea(BlockPos machinePos, ServerWorld world) {
-        BlockPos start = machinePos.add(-width / 2, 0, -depth / 2); // Bottom-left front corner
-        BlockPos end = machinePos.add(width / 2, height, depth / 2); // Top-right back corner
-
-        for (int i = 0; i < 4; i++) {
-            BlockPos topCorner = getCorner(start, end, i, true);
-            BlockPos bottomCorner = getCorner(start, end, i, false);
-            LineDrawer.drawLine(new DustParticleEffect(new Vector3f(0F, 0.75F, 1F), 0.75F), topCorner, bottomCorner, world);
-        }
-
-        for (int i = 0; i < 4; i++) {
-            BlockPos startCorner = getCorner(start, end, i, true);
-            BlockPos endCorner = getCorner(start, end, (i + 1) % 4, true);
-            LineDrawer.drawLine(new DustParticleEffect(new Vector3f(0F, 0.75F, 1F), 0.75F), startCorner, endCorner, world);
-        }
-
-        for (int i = 0; i < 4; i++) {
-            BlockPos startCorner = getCorner(start, end, i, false);
-            BlockPos endCorner = getCorner(start, end, (i + 1) % 4, false);
-            LineDrawer.drawLine(new DustParticleEffect(new Vector3f(0F, 0.75F, 1F), 0.75F), startCorner, endCorner, world);
-        }
-    }
-
-    private BlockPos getCorner(BlockPos start, BlockPos end, int cornerIndex, boolean isTop) {
-        int x = (cornerIndex == 0 || cornerIndex == 3) ? start.getX() : end.getX();
-        int z = (cornerIndex == 0 || cornerIndex == 1) ? start.getZ() : end.getZ();
-        int y = isTop ? end.getY() : start.getY();
-        return new BlockPos(x, y, z);
+        return state.isIn(BlockTags.DIRT);
     }
 
     @Override
     public Text infoOnClicked() {
-        visualizeScanArea(pos, (ServerWorld) world);
+        LineDrawer.visualizeScanArea(pos, (ServerWorld) world, boxSize);
         return super.infoOnClicked();
     }
 
