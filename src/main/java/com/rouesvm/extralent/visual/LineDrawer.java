e55b100ee8500a2f760e6df8d793e9af8f5eac51
drivlo <rouesvmnowfree@gmail.com>
2024-12-28 17:04:26 +0800

    Moved visualizeScanArea to LineDrawer.

--- src/main/java/com/rouesvm/extralent/visual/LineDrawer.java
+++ src/main/java/com/rouesvm/extralent/visual/LineDrawer.java
@@ -1,8 +1,11 @@
 package com.rouesvm.extralent.visual;
 
+import net.minecraft.particle.DustParticleEffect;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3i;
+import org.joml.Vector3f;
 
 public class LineDrawer {
     public static <T extends ParticleEffect> void drawLine(T particleType, BlockPos start, BlockPos end, ServerWorld world) {
@@ -23,5 +26,35 @@ public class LineDrawer {
 
             world.spawnParticles(particleType, x, y, z, 0, 0, 0, 0, 0);
         }
+    }
+
+    public static void visualizeScanArea(BlockPos machinePos, ServerWorld world, Vec3i size) {
+        BlockPos start = machinePos.add(-size.getX() / 2, 0, -size.getZ() / 2); // Bottom-left front corner
+        BlockPos end = machinePos.add(size.getX() / 2, size.getY(), size.getZ() / 2); // Top-right back corner
+
+        for (int i = 0; i < 4; i++) {
+            BlockPos topCorner = getCorner(start, end, i, true);
+            BlockPos bottomCorner = getCorner(start, end, i, false);
+            LineDrawer.drawLine(new DustParticleEffect(new Vector3f(0F, 0.75F, 1F), 0.75F), topCorner, bottomCorner, world);
+        }
+
+        for (int i = 0; i < 4; i++) {
+            BlockPos startCorner = getCorner(start, end, i, true);
+            BlockPos endCorner = getCorner(start, end, (i + 1) % 4, true);
+            LineDrawer.drawLine(new DustParticleEffect(new Vector3f(0F, 0.75F, 1F), 0.75F), startCorner, endCorner, world);
+        }
+
+        for (int i = 0; i < 4; i++) {
+            BlockPos startCorner = getCorner(start, end, i, false);
+            BlockPos endCorner = getCorner(start, end, (i + 1) % 4, false);
+            LineDrawer.drawLine(new DustParticleEffect(new Vector3f(0F, 0.75F, 1F), 0.75F), startCorner, endCorner, world);
+        }
+    }
+
+    private static BlockPos getCorner(BlockPos start, BlockPos end, int cornerIndex, boolean isTop) {
+        int x = (cornerIndex == 0 || cornerIndex == 3) ? start.getX() : end.getX();
+        int z = (cornerIndex == 0 || cornerIndex == 1) ? start.getZ() : end.getZ();
+        int y = isTop ? end.getY() : start.getY();
+        return new BlockPos(x, y, z);
     }
 }
